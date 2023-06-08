deac1b96bf375447a39e74706f15849b3b91ed1e
Muhammad Azka <hello.mazka@gmail.com>
2023-06-09 00:56:02 +0700

    refactor: added some codes while in development stage

---
+++ src/pages/builder/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Builder = () => {
+  return (
+    <main className="flex flex-col justify-center items-center my-6">
+      
+    </main>
+  );
+}
+
+export default Builder
